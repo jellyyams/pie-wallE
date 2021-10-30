b4804b57774bf42405c9cd8f3358bf6c56eca972
Julia (Jules) Brettle <57976312+julesbrettle@users.noreply.github.com>
2021-10-30 13:26:32 -0400

    Update wall-e.ino

--- wall-e/wall-e.ino
+++ wall-e/wall-e.ino
@@ -1,4 +1,6 @@
 
+#include <Servo.h>
+
 // servo setup
 Servo leftEyeServo;     // create servo object to control wall-e's left eye and head tilt (in conjunction with rightEyeServo)
 Servo rightEyeServo;    // create servo object to control wall-e's right eye and head tilt (in conjunction with leftEyeServo)
@@ -11,27 +13,40 @@ Servo rightArmServo;    // create servo object to control wall-e's right arm rot
 int leftEyePos;
 int rightEyePos;
 int headPanPos;
-int topNeckPos;
 int botNeckPos;
 int leftArmPos;
 int rightArmPos;
 
 int currBPM = 60;    // initialize music BPM. Update with change in music
-int stepLength;      // calculated from bpm
-int beatStart = 0;
-int currMili;
-int nextMilli = beatStart;        
-int allowedError = 50; // milliseconds we can be off by
+unsigned long stepLength = 100;      // in milliseconds, calculated from bpm
+unsigned long beatStart = 0;
+unsigned long currMilli = 0;
+unsigned long nextMilli = stepLength;        
+unsigned long allowedError = 50; // milliseconds we can be off by
+
+int topNeckMin = 10;
+int topNeckMax = 110;
+int topNeckPos = topNeckMin;
+int topNeckStep = 5;
+int nextTopNeck = topNeckPos + topNeckStep;
+unsigned long topNeckLastSwitch = 0;
+float topNeckRealBPM = 0;
+
+
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600); // open the serial port at 9600 bps:
-  leftEyeServo.attach(9);  // attaches the servo on pin __ to the leftEyeServo object
-  rightEyeServo.attach(10);  // attaches the servo on pin __ to the rightEyeServo object
-  headPanServo.attach(11);  // attaches the servo on pin __ to the headPanServo object
-  topNeckServo.attach(12);  // attaches the servo on pin __ to the topNeckServo object
-  botNeckServo.attach(13);  // attaches the servo on pin __ to the botNeckServo object
+  Serial.begin(230400); // open the serial port at 9600 bps:
+//  leftEyeServo.attach(9);  // attaches the servo on pin __ to the leftEyeServo object
+//  rightEyeServo.attach(10);  // attaches the servo on pin __ to the rightEyeServo object
+//  headPanServo.attach(11);  // attaches the servo on pin __ to the headPanServo object
+  topNeckServo.attach(9);  // attaches the servo on pin __ to the topNeckServo object
+//  botNeckServo.attach(13);  // attaches the servo on pin __ to the botNeckServo object
 
+}
+void serial_flush(void) {
+  // manually flushes the serial input so that user input values aren't misread
+  while (Serial.available()) Serial.read();
 }
 
 void calcStepLength() {
@@ -40,6 +55,7 @@ void calcStepLength() {
 
 void nodHead() {
   // calculates and sends servo commands for one step of the nodHead dance move
+  
 }
 
 void shakeHead() {
@@ -47,33 +63,54 @@ void shakeHead() {
 }
 
 void bobHead() {
-  
+  // calculates and sends servo commands for one step of the bobHead dance move
 }
 
 void swingArms() {
-  
+  // calculates and sends servo commands for one step of the swingArms dance move
 }
 
 void tiltHead() {
-  
+  // calculates and sends servo commands for one step of the tiltHead dance move
 }
 
 
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  delay(110);
   // check the current time and wait until nextMili to start the next step
   currMilli = millis();
-  while (currMilli() < nextMilli) {
+  Serial.print("millis="); Serial.println(currMilli);
+
+  while (currMilli <= nextMilli) {
     // wait for stepLength before starting the next step
   }
-  if (abs(currMilli()-nextMilli) > allowedError) {
-    Serial.print("Error: running "); Serial.print(currMilli()-nextMilli)); Serial.println(" ms behind");
+  if (abs(currMilli-nextMilli) > allowedError) {
+    Serial.print("Error: running "); Serial.print(currMilli-nextMilli); Serial.println(" ms behind");
   }
   nextMilli = currMilli + stepLength;
 
+  Serial.print("millis="); Serial.println(currMilli);
+  Serial.print("topNeckPos="); Serial.println(topNeckPos);
+  Serial.print("topNeckRealBPM="); Serial.println(topNeckRealBPM);
+
+  
   // run or continue these dance moves
+  topNeckServo.write(nextTopNeck);
+  topNeckPos = nextTopNeck;
+  nextTopNeck = topNeckPos + topNeckStep;
+
+  if (nextTopNeck > topNeckMax || nextTopNeck < topNeckMin) {
+    topNeckStep = -1*topNeckStep;
+    nextTopNeck = nextTopNeck + 2*topNeckStep;
+    topNeckRealBPM = 1/((currMilli-topNeckLastSwitch)/1000/60);
+    topNeckLastSwitch = currMilli;
+  }
+
+  
+  
+  
 
   
   
