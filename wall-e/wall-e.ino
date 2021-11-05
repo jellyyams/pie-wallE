936115731c96c29a2524bf42fbf7f89714e5a218
Julia (Jules) Brettle <57976312+julesbrettle@users.noreply.github.com>
2021-11-05 10:30:16 -0400

    Clean up and comment code

--- wall-e/wall-e.ino
+++ wall-e/wall-e.ino
@@ -13,7 +13,23 @@ Servo botNeckServo;     // create servo object to control wall-e's head bobbing 
 Servo leftArmServo;     // create servo object to control wall-e's left arm rotation
 Servo rightArmServo;    // create servo object to control wall-e's right arm rotation
 
-bool waitToStart = true;
+
+// Set program parameters
+
+int currBPM = 70;    // 70-190 initialize music BPM. Will update with change in music based on mic input
+bool waitToStart = true; // wait for something to send in the Serial input before starting (in order to manually start on the beat)
+
+int minDelay = 110;
+int assumedServoSpeed = 8; // ms/deg
+
+int leftEyeRangeMax = 55;
+int leftEyeCenter = 150;
+
+int rightEyeRangeMax = 55;
+int rightEyeCenter = 30;
+
+
+// Initalize global variables
 
 int headPanPos;
 int topNeckPos;
@@ -21,21 +37,13 @@ int botNeckPos;
 int leftArmPos;
 int rightArmPos;
 
-int currBPM = 70;    // 70-190 initialize music BPM. Eventually will update with change in music
 unsigned long stepLength = 600;      // in milliseconds, calculated from bpm
 unsigned long beatStart = 0;
 unsigned long oldMilli = 0;
 unsigned long currMilli = 0;
 unsigned long nextMilli = stepLength;        
 unsigned long allowedError = 50; // milliseconds we can be off by
-
 unsigned long realStepLength = stepLength;
-
-int minDelay = 110;
-int assumedServoSpeed = 8; // ms/deg
-
-int leftEyeRangeMax = 55;
-int leftEyeCenter = 150;
 
 double leftEyeRange = 50;
 int leftEyeMin = leftEyeCenter-leftEyeRange;
@@ -46,10 +54,6 @@ int leftEyeDir = 1;
 int nextLeftEye = leftEyePos + leftEyeDir*leftEyeStep;
 unsigned long leftEyeLastSwitch = 0;
 double leftEyeRealBPM = 0;
-
-
-int rightEyeRangeMax = 55;
-int rightEyeCenter = 30;
 
 double rightEyeRange = 50;
 int rightEyeMin = rightEyeCenter-rightEyeRange;
@@ -66,6 +70,8 @@ double rightEyeRealBPM = 0;
 
 void setup() {
   // put your setup code here, to run once:
+
+  // Set up serial and attach servos
   Serial.begin(9600); // open the serial port at 9600 bps:
   leftEyeServo.attach(3);  // attaches the servo on pin __ to the leftEyeServo object
   rightEyeServo.attach(5);  // attaches the servo on pin __ to the rightEyeServo object
@@ -73,16 +79,16 @@ void setup() {
 //  topNeckServo.attach(9);  // attaches the servo on pin __ to the topNeckServo object
 //  botNeckServo.attach(13);  // attaches the servo on pin __ to the botNeckServo object
 
+  // Move servos to starting positions
   leftEyePos = leftEyeCenter;
   rightEyePos = rightEyeCenter;
-
   leftEyeServo.write(leftEyePos);
   rightEyeServo.write(rightEyePos);
   
-
+  // Calculate the starting ranges and step length based on BPM
   calcStepLengthAndRanges();
 
-
+  // Wait to begin main loop until user inputs something if waitToStart = true (in order to manually start on beat)
   if (waitToStart) {
     while (Serial.available() == 0) {
       // Wait for User to Input Data
@@ -91,22 +97,25 @@ void setup() {
   }
 
 }
+
 void serial_flush(void) {
   // manually flushes the serial input so that user input values aren't misread
   while (Serial.available()) Serial.read();
 }
 
 void calcStepLengthAndRanges() {
+
+  // Calculate and set leftEye movement parameters
   leftEyeRange =  60.0*1000.0/(double(assumedServoSpeed)*double(currBPM))/2 ;
   leftEyeMin = leftEyeCenter-leftEyeRange/2;
   leftEyeMax = leftEyeCenter+leftEyeRange/2;
   leftEyeStep = int(leftEyeRange);
   nextLeftEye = leftEyePos + leftEyeDir*leftEyeStep;
-
   if (int(leftEyeRange) > leftEyeRangeMax) {
     leftEyeRange = leftEyeRangeMax;
   }
 
+  // Calculate and set rightEye movement parameters
   rightEyeRange =  60.0*1000.0/(double(assumedServoSpeed)*double(currBPM))/2 ;
   rightEyeMin = rightEyeCenter-rightEyeRange/2;
   rightEyeMax = rightEyeCenter+rightEyeRange/2;
@@ -116,7 +125,8 @@ void calcStepLengthAndRanges() {
     rightEyeRange = rightEyeRangeMax;
   }
 
-  stepLength = leftEyeRange*assumedServoSpeed;
+  // Set loop delay, and check that it's not so small as to overload the arduino
+  stepLength = leftEyeRange*assumedServoSpeed;  // eventually need to set up for sub-range movement steps
   if (stepLength < minDelay) {
     Serial.println("Error: stepLength is too small.");
     stepLength = minDelay;
@@ -161,22 +171,18 @@ void updateBPM(int val){
 
 void loop() {
   microphoneValue = analogRead(microphonePin); 
-
   updateBPM(microphoneValue); 
-  
-//   if(!Serial) {  //check if Serial is available... if not,
-//    Serial.end();      // close serial port
-//    delay(100);        //wait 100 millis
-//    Serial.begin(250000); // reenable serial again
-//    Serial.println("Serial lost and reenabled");
-//   }
+  calcStepLengthAndRanges();
 
-  // check the current time and wait until nextMili to start the next step
+  // check the current time
   currMilli = millis();
+  
+
+  
 //  Serial.print("millis="); Serial.println(currMilli);
 
-//  calcStepLengthAndRanges();
 
+  // wait until nextMilli to start the movement step
 //  while (currMilli <= nextMilli) {
 //    // wait for stepLength before starting the next step
 //    delay(minDelay);
@@ -184,10 +190,14 @@ void loop() {
 //  if (abs(currMilli-nextMilli) > allowedError) {
 //    Serial.print("Error: running "); Serial.print(currMilli-nextMilli); Serial.println(" ms behind");
 //  }
+
+  // calculate how far off we are from the nextMilli we were aiming for
   realStepLength = currMilli - oldMilli;
   oldMilli = currMilli;
   nextMilli = currMilli + stepLength;
 
+  // Print current time and movement parameters for debugging
+  
 //  Serial.print("currMilli="); Serial.println(currMilli);
 //  Serial.print("stepLength="); Serial.println(stepLength);
 //  Serial.print("realStepLength="); Serial.println(realStepLength);
@@ -204,48 +214,43 @@ void loop() {
   Serial.print("rightEyeMax="); Serial.println(rightEyeMax);
   Serial.print("rightEyeRange="); Serial.println(rightEyeRange);
   
-
-  
-
+  // if next position is outside of bounds, send back one step the opposite direction
   if (nextLeftEye > leftEyeMax ) {
     leftEyeDir = -1;
     nextLeftEye = leftEyeMax-leftEyeStep;
-    leftEyeRealBPM = (1/double(currMilli-leftEyeLastSwitch))*1000*60/2;
+    leftEyeRealBPM = (1/double(currMilli-leftEyeLastSwitch))*1000*60/2; // calculate the actual time it took to complete the whole range
     leftEyeLastSwitch = currMilli;
   } else if (nextLeftEye < leftEyeMin) {
     leftEyeDir = 1;
     nextLeftEye = leftEyeMin+leftEyeStep;
-    leftEyeRealBPM = (1/double(currMilli-leftEyeLastSwitch))*1000*60/2;
+    leftEyeRealBPM = (1/double(currMilli-leftEyeLastSwitch))*1000*60/2; // calculate the actual time it took to complete the whole range
     leftEyeLastSwitch = currMilli;
   }
 
+  // write new servo position
   leftEyeServo.write(nextLeftEye);
   leftEyePos = nextLeftEye;
   nextLeftEye = leftEyePos + leftEyeDir*leftEyeStep;
 
-
+  // if next position is outside of bounds, send back one step the opposite direction
   if (nextRightEye > rightEyeMax ) {
     rightEyeDir = -1;
     nextRightEye = rightEyeMax-rightEyeStep;
-    rightEyeRealBPM = (1/double(currMilli-rightEyeLastSwitch))*1000*60/2;
+    rightEyeRealBPM = (1/double(currMilli-rightEyeLastSwitch))*1000*60/2; // calculate the actual time it took to complete the whole range
     rightEyeLastSwitch = currMilli;
   } else if (nextRightEye < rightEyeMin) {
     rightEyeDir = 1;
     nextRightEye = rightEyeMin+rightEyeStep;
-    rightEyeRealBPM = (1/double(currMilli-rightEyeLastSwitch))*1000*60/2;
+    rightEyeRealBPM = (1/double(currMilli-rightEyeLastSwitch))*1000*60/2; // calculate the actual time it took to complete the whole range
     rightEyeLastSwitch = currMilli;
   }
 
+  // write new servo position
   rightEyeServo.write(nextRightEye);
   rightEyePos = nextRightEye;
   nextRightEye = rightEyePos + rightEyeDir*rightEyeStep;
 
   
   
-  
-
-  
-  
-
   delay(stepLength);
 }
