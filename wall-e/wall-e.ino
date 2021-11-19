781963a4e8cab1b939085b53babbb85170be4e76
Julia (Jules) Brettle <57976312+julesbrettle@users.noreply.github.com>
2021-11-19 11:34:12 -0500

    Update wall-e.ino

--- wall-e/wall-e.ino
+++ wall-e/wall-e.ino
@@ -1,5 +1,3 @@
-
-
 #include <Wire.h>
 #include <Servo.h>
 #include <Adafruit_PWMServoDriver.h>
@@ -20,35 +18,20 @@ double range[7];
 double closestBPM[7];
 double minPos[7];
 double maxPos[7];
+double curPos[7];
 double nextPos[7];
 unsigned long lastSwitch[7];
 double realBPM[7];
 double realVel[7];
 
+double maxError = 1.0;
+double minStep = 0.1;
+
 
 // Servo driver setup
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 #define SERVO_FREQ 50 // Analog servos run at ~50 Hz updates
-uint8_t leftEyePort = 0;
-uint8_t rightEyePort = 1;
-uint8_t headPanPort = 2;
-uint8_t topNeckPort = 3;
-uint8_t botNeckPort = 4;
-uint8_t leftArmPort = 5;
-uint8_t rightArmPort = 6;
 
-// Direct control servo setup
-Servo leftEyeServo;     // create servo object to control wall-e's left eye and head tilt (in conjunction with rightEyeServo)
-Servo rightEyeServo;    // create servo object to control wall-e's right eye and head tilt (in conjunction with leftEyeServo)
-Servo headPanServo;     // create servo object to control wall-e's head shaking
-Servo topNeckServo;     // create servo object to control wall-e's head nodding
-Servo botNeckServo;     // create servo object to control wall-e's head bobbing (in conjunction with topNeckServo)
-Servo leftArmServo;     // create servo object to control wall-e's left arm rotation
-Servo rightArmServo;    // create servo object to control wall-e's right arm rotation
-
-// Set program parameters
-
-int currBPM = 70;    // 70-190 initialize music BPM. Will update with change in music based on mic input
 bool waitToStart = true; // wait for something to send in the Serial input before starting (in order to manually start on the beat)
 
 int minDelay = 110;
@@ -169,37 +152,129 @@ void calcFullSpeedVals() {
     stepLength = minDelay;
   }
   
-  
-  
 }
 
-void setServo(uint8_t servoPort, double deg) {
-  // calculate pulselen from deg
-  pulselen = map(deg, 0, 180, SERVOMIN, SERVOMAX);
+void matchBPM(uint8_t s, double BPM) {
+  int newTimeMult;
+  double newStep;
+  double newRange;
+  double newBPM;
+  double newError;
+  double closestBPM;
+  double closestError=100;
+  
+  newTimeMult = 1;
+  while (newTimeMult <= maxTimeMult[s]) {
+    Serial.println(newTimeMult);
+    if (abs(closestError) < maxError) {
+      return;
+    }
+    newStep = stepFromTimeMult(newTimeMult,maxVel[s]);
+    Serial.print("newStep="); Serial.println(newStep);
+    newRange = calcRange(BPM, newTimeMult, newStep);
+    Serial.print("newRange="); Serial.println(newRange);
+    if (newRange > maxRange[s]) {
+      newRange = maxRange[s];
+      Serial.print("Too large. newRange="); Serial.println(newRange);
+      newStep = calcStep(BPM, newTimeMult, newRange);
+      Serial.print("newStep="); Serial.println(newStep);
+    }
+    newBPM = calcBPM(newTimeMult, newStep, newRange);
+    Serial.print("newBPM="); Serial.println(newBPM);
+    newError = newBPM-BPM;
+    Serial.print("newError="); Serial.println(newError);
+    if (newError > -0.01 && abs(newError) < abs(closestError) && newStep >= minStep) {
+      closestError = BPM-newBPM;
+      
+      timeMult[s] = newTimeMult;
+      stepDist[s] = newStep;
+      range[s] = newRange;
+      minPos[s] = center[s]-range[s]/2;
+      maxPos[s] = center[s]+range[s]/2;
+      closestBPM[s] = newBPM;
+    }
+    newTimeMult++;
+  }
+}
+double stepFromTimeMult(double newTimeMult, double vel) {
+  return vel*double(newTimeMult*baseTime)/1000.0;
+}
+double calcStep(double bpm,int newTimeMult, double newRange) {
+  return newRange*bpm*double(newTimeMult*baseTime)/(60.0*1000.0);
+}
+double calcRange(double bpm,int newTimeMult, double newStep) {
+  return 60.0*1000.0*newStep/(bpm*double(newTimeMult*baseTime));
+}
+double calcBPM(int newTimeMult, double newStep, double newRange) {
+  return newStep*1000.0*60.0/(newRange*double(newTimeMult*baseTime));
+}
+double calcTimeMult(double bpm, double newStep, double newRange) {
+  return 60.0*1000.0/(newRange*bpm*double(baseTime));
+}
 
+void adjustBPM(uint8_t s, double reduction){
+  if (realBPM[s] < bpm[s]) {
+    matchBPM(s, bpm[s]+(bpm[s]-realBPM[s]));
+  }
+}
+void calcNextPos(uint8_t s) {
+  // calc next position
+  nextPos[s] = curPos[s] + dir[s]*stepDist[s];
+  // switch directions and record real measurements at each end of range
+  if (nextPos[s] > maxPos[s] ) {
+    dir[s] = -1;
+    nextPos[s] = maxPos[s]- stepDist[s];
+    realBPM[s] = (1/double(currMilli-lastSwitch[s]))*1000*60; // calculate the actual time it took to complete the whole range
+    realVel[s] = (1/double(currMilli-lastSwitch[s]))*1000*range[s]; // calculate the actual angular speed (deg/s)
+    lastSwitch[s] = currMilli;
+  } else if (nextPos[s] < minPos[s]) {
+    dir[s] = 1;
+    nextPos[s] = maxPos[s]- stepDist[s];
+    realBPM[s] = (1/double(currMilli-lastSwitch[s]))*1000*60; // calculate the actual time it took to complete the whole range
+    realVel[s] = (1/double(currMilli-lastSwitch[s]))*1000*range[s]; // calculate the actual angular speed (deg/s)
+    lastSwitch[s] = currMilli;
+    adjustBPM(s,1);
+  }
+}
+
+void printVals(uint8_t s) {
+  Serial.print("s"); Serial.print(s); Serial.print(" stepDist="); Serial.println(stepDist[s]);
+//  Serial.print("s"); Serial.print(s); Serial.print(" curPos="); Serial.println(curPos[s]);
+//  Serial.print("s"); Serial.print(s); Serial.print(" range="); Serial.println(range[s]);
+//  Serial.print("s"); Serial.print(s); Serial.print(" minPos="); Serial.println(minPos[s]);
+//  Serial.print("s"); Serial.print(s); Serial.print(" maxPos="); Serial.println(maxPos[s]);
+  Serial.print("s"); Serial.print(s); Serial.print(" realBPM="); Serial.println(realBPM[s]);
+//  Serial.print("s"); Serial.print(s); Serial.print(" realVel="); Serial.println(realVel[s]);
+}
+
+void setServo(uint8_t s) {
+  // calculate pulselen from deg (of nextPos)
+  pulselen = map(nextPos[s], 0, 180, SERVOMIN, SERVOMAX);
   // set PWM to pulselen
-  pwm.setPWM(servoPort, 0, pulselen);
+  pwm.setPWM(s, 0, pulselen);
+  // update curPos
+  curPos[s] = nextPos[0];
 }
+
 
 void nodHead() {
-  // calculates and sends servo commands for one step of the nodHead dance move
-  
+  // sends servo commands for one step of the nodHead dance move
 }
 
 void shakeHead() {
-  // calculates and sends servo commands for one step of the shakeHead dance move
+  // sends servo commands for one step of the shakeHead dance move
 }
 
 void bobHead() {
-  // calculates and sends servo commands for one step of the bobHead dance move
+  // sends servo commands for one step of the bobHead dance move
 }
 
 void swingArms() {
-  // calculates and sends servo commands for one step of the swingArms dance move
+  // sends servo commands for one step of the swingArms dance move
 }
 
 void tiltHead() {
-  // calculates and sends servo commands for one step of the tiltHead dance move
+  // sends servo commands for one step of the tiltHead dance move
 }
 
 void updateBPM(int val){
@@ -213,27 +288,14 @@ void updateBPM(int val){
   }
 }
 
-
 void loop() {
-  microphoneValue = analogRead(microphonePin); 
-  updateBPM(microphoneValue); 
+  
+  // update bpm from beat sensing
+//  microphoneValue = analogRead(microphonePin); 
+//  updateBPM(microphoneValue); 
 
   // check the current time
   currMilli = millis();
-  
-
-  
-//  Serial.print("millis="); Serial.println(currMilli);
-
-
-  // wait until nextMilli to start the movement step
-//  while (currMilli <= nextMilli) {
-//    // wait for stepLength before starting the next step
-//    delay(minDelay);
-//  }
-//  if (abs(currMilli-nextMilli) > allowedError) {
-//    Serial.print("Error: running "); Serial.print(currMilli-nextMilli); Serial.println(" ms behind");
-//  }
 
   // calculate how far off we are from the nextMilli we were aiming for
   realStepLength = currMilli - oldMilli;
@@ -241,60 +303,17 @@ void loop() {
   nextMilli = currMilli + stepLength;
 
   // Print current time and movement parameters for debugging
-  
 //  Serial.print("currMilli="); Serial.println(currMilli);
-//  Serial.print("stepLength="); Serial.println(stepLength);
 //  Serial.print("realStepLength="); Serial.println(realStepLength);
-//
-//  Serial.print("leftEyePos="); Serial.println(leftEyePos);
-//  Serial.print("leftEyeRealBPM="); Serial.println(leftEyeRealBPM);
-//  Serial.print("leftEyeMin="); Serial.println(leftEyeMin);
-//  Serial.print("leftEyeMax="); Serial.println(leftEyeMax);
-//  Serial.print("leftEyeRange="); Serial.println(leftEyeRange);
 
-   Serial.print("rightEyePos="); Serial.println(rightEyePos);
-//  Serial.print("rightEyeRealBPM="); Serial.println(rightEyeRealBPM);
-  Serial.print("rightEyeMin="); Serial.println(rightEyeMin);
-  Serial.print("rightEyeMax="); Serial.println(rightEyeMax);
-  Serial.print("rightEyeRange="); Serial.println(rightEyeRange);
-  
-  // if next position is outside of bounds, send back one step the opposite direction
-  if (nextLeftEye > leftEyeMax ) {
-    leftEyeDir = -1;
-    nextLeftEye = leftEyeMax-leftEyeStep;
-    leftEyeRealBPM = (1/double(currMilli-leftEyeLastSwitch))*1000*60/2; // calculate the actual time it took to complete the whole range
-    leftEyeLastSwitch = currMilli;
-  } else if (nextLeftEye < leftEyeMin) {
-    leftEyeDir = 1;
-    nextLeftEye = leftEyeMin+leftEyeStep;
-    leftEyeRealBPM = (1/double(currMilli-leftEyeLastSwitch))*1000*60/2; // calculate the actual time it took to complete the whole range
-    leftEyeLastSwitch = currMilli;
+  for (uint8_t i; i <=1; i++) {
+    calcNextPos(0);
+    printVals(0);
+    setServo(0); // set servo to nextPos, update curPos to nextPos
   }
-
-  // write new servo position
-  leftEyeServo.write(nextLeftEye);
-  leftEyePos = nextLeftEye;
-  nextLeftEye = leftEyePos + leftEyeDir*leftEyeStep;
-
-  // if next position is outside of bounds, send back one step the opposite direction
-  if (nextRightEye > rightEyeMax ) {
-    rightEyeDir = -1;
-    nextRightEye = rightEyeMax-rightEyeStep;
-    rightEyeRealBPM = (1/double(currMilli-rightEyeLastSwitch))*1000*60/2; // calculate the actual time it took to complete the whole range
-    rightEyeLastSwitch = currMilli;
-  } else if (nextRightEye < rightEyeMin) {
-    rightEyeDir = 1;
-    nextRightEye = rightEyeMin+rightEyeStep;
-    rightEyeRealBPM = (1/double(currMilli-rightEyeLastSwitch))*1000*60/2; // calculate the actual time it took to complete the whole range
-    rightEyeLastSwitch = currMilli;
-  }
-
-  // write new servo position
-  rightEyeServo.write(nextRightEye);
-  rightEyePos = nextRightEye;
-  nextRightEye = rightEyePos + rightEyeDir*rightEyeStep;
-
   
-  
-  delay(stepLength);
+ adjustedDelay = int(nextMilli-millis());
+ delay(adjustedDelay);
+//  delay(timestep);
+
 }
